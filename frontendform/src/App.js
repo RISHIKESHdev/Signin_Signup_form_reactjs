66b60c2ea723cc3b177ccdcaf64e7ec9d39db25d
RISHIKESHdev <rishikeshhappydeveloper@gmail.com>
2022-07-03 16:10:51 +0530

    front end without data layer completed

---
+++ frontendform/src/App.js
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [isSignup,setisSignup]=useState(false)
+  return (
+    <body>
+      <div className="formDiv">
+        <div className="formOptions">
+          <div className={`${isSignup ? "" :"activeSign"}`} onClick={()=>{setisSignup(false)}}>SIGN IN</div>
+          <div className={`${isSignup ? "activeSign" :""}`} onClick={()=>{setisSignup(true)}}>SIGN UP</div>
+        </div>
+          {(isSignup)?(<>
+          <form className="formBody">
+            <label htmlFor="b1" >Name</label>
+            <input type="text" id="b1" required="required"/>
+            <label htmlFor="b2" >Email-id</label>
+            <input type="text" id="b2" required="required"/>
+            <label htmlFor="b3" >Password</label>
+            <input type="text" id="b3" required="required"/>
+            <label htmlFor="b4" >Password Check</label>
+            <input type="text" id="b4" required="required"/>
+            <button>SIGN UP</button>
+          </form>
+            
+          </>):(<>
+            <label htmlFor="b2" >Email-id</label>
+            <input type="text" id="b2" required="required"/>
+            <label htmlFor="b3" >Password</label>
+            <input type="text" id="b3" required="required"/>
+            <button>SIGN IN</button>
+          </>)}
+        </div>
+    </body>
+  );
+}
+
+export default App;
